fff496c58427d04529cd1d00bf2489c63191d76e
Jason <jasonjones@salesforce.com>
2017-01-13 16:11:13 -0800

    Add skeleton code to connect to mongodb via docker

---
+++ src/config/dbmanager.ts
@@ -0,0 +1,31 @@
+import * as mongoose from 'mongoose';
+import { DbEnv } from './enums';
+
+import config from './config';
+
+class DbManager {
+
+    private connection: mongoose.Connection;
+
+    constructor(env: DbEnv) {
+        if (env === DbEnv.DOCKER) {
+            mongoose.connect(`mongodb://${config.db.host}/${config.db.name}`);
+            this.connection = mongoose.connection;
+            this.initHandlers();
+        } else {
+            console.log('Mongodb not install on local machine');
+        }
+    }
+
+    private initHandlers() : void {
+        console.log('Initializing db handlers...');
+
+        this.connection.once('open', () => {
+            console.log('Connected to mongo container...');
+        });
+
+        this.connection.on('error', console.error.bind(console, 'connection error'));
+    }
+}
+
+export default DbManager;
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,10 @@
+import { DbEnv } from './config/enums';
 import config from './config/config';
+import DbManager from './config/dbmanager';
 
 import app from './config/app';
+
+let dbmanager = new DbManager(DbEnv.DOCKER);
 
 app.listen(config.port, () => {
     console.log('node (typscript) server listening on port ' + config.port);
