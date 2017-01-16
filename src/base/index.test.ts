6c9bd7d8e42c71121016591965a27953704713c1
Jason <jasonjones@salesforce.com>
2017-01-15 18:37:19 -0800

    Add catch block to in index test

--- src/base/index.test.ts
+++ src/base/index.test.ts
@@ -10,17 +10,25 @@ const expect = chai.expect;
 describe('Base Route', () => {
 
     it('should be json', () => {
-        chai.request(app).get('/')
-        .then(res => {
-            expect(res.type).to.eql('application/json');
-        });
+        chai.request(app)
+            .get('/')
+            .then((res) => {
+                expect(res.type).to.eql('application/json');
+            })
+            .catch((err) => {
+                throw err;
+            });
     });
 
     it('should have message property that is a string', () => {
-        chai.request(app).get('/')
-        .then(res => {
-            expect(res).to.have.property('message').that.is.a('string');
-        });
+        chai.request(app)
+            .get('/')
+            .then((res) => {
+                expect(res.body).to.have.property('message').that.is.a('string');
+            })
+            .catch((err) => {
+                throw err;
+            });
     });
 
 });
