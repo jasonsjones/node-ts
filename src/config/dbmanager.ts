bb4ac9f66502a2e43e5aaffe1aa04d95f6c4981e
Jason <jasonjones@salesforce.com>
2017-01-16 21:09:28 -0800

    Small Refactor of dbmanager.ts

--- src/config/dbmanager.ts
+++ src/config/dbmanager.ts
@@ -8,16 +8,16 @@ class DbManager {
     private connection: mongoose.Connection;
 
     constructor(env: DbEnv) {
+        (<any>mongoose).Promise = global.Promise;
         if (env === DbEnv.DOCKER) {
-            mongoose.connect(`mongodb://${config.db.host}/${config.db.name}`);
-            this.connection = mongoose.connection;
+            this.connectToDockerDb();
             this.initHandlers();
         } else {
             console.log('Mongodb not install on local machine');
         }
     }
 
-    private initHandlers() : void {
+    private initHandlers(): void {
         console.log('Initializing db handlers...');
 
         this.connection.once('open', () => {
@@ -25,6 +25,11 @@ class DbManager {
         });
 
         this.connection.on('error', console.error.bind(console, 'connection error'));
+    }
+
+    private connectToDockerDb(): void {
+        mongoose.connect(`mongodb://${config.db.host}/${config.db.name}`);
+        this.connection = mongoose.connection;
     }
 }
 
