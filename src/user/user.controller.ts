1370804c20d5ff41eeb9b27bced747c39a2d3c76
Jason <jasonjones@salesforce.com>
2017-01-19 14:08:53 -0800

    Refactor and remove commented out code

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,37 +1,24 @@
 import { Request, Response } from 'express';
-import UserModel from './user.model';
+import { UserModel } from './user.model';
+
+const User = new UserModel().getModel().user;
 
 export function getUsers(req: Request, res: Response): void {
     res.json({message: 'Getting all users...'});
 }
 
 export function seedUsers(req: Request, res: Response): void {
-    console.log('UserModel seedUsers...');
-    // console.log(UserModel.find({}).exec().then(()=>{console.log('query promise...')}));
-    UserModel.find({}).exec()
+    User.find({}).exec()
         .then(collection => {
-            console.log(collection);
             if (collection && collection.length === 0) {
                 // create users here...
                 res.json({message: 'seeding users in database'})
             }
         },
-        () => {console.log('reject')})
+        () => {
+            console.log('promise rejected');
+        })
         .catch(err => {
             console.log('error: ' + err);
         });
-    // UserModel.find({}).exec((err, collection) => {
-    //         if (err) {
-    //             console.log('Ooops...there is an error getting the users...');
-    //             res.status(500).send(err);
-    //         }
-
-    //         if (collection && collection.length === 0) {
-    //             // create users here...
-    //             res.json({message: 'seeding users in database'})
-    //         } else {
-    //             console.log('database already contains a collection of users...');
-    //             res.json({message: 'database contains users...'});
-    //         }
-    //     });
 }
