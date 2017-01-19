29fe4ed23db2085d3a1094df275f575c7d8e5256
Jason <jasonjones@salesforce.com>
2017-01-19 14:06:15 -0800

    Remove DbManager dependency from server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,6 @@
-import { DbEnv } from './common/enums';
 import config from './config/config';
-import DbManager from './config/dbmanager';
-
 import app from './config/app';
-
-let dbmanager = DbManager.getInstance(DbEnv.DOCKER);
 
 app.listen(config.port, () => {
     console.log('node (typscript) server listening on port ' + config.port);
-})
+});
