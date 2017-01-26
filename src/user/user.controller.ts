20048322d785c4dce1e6c93e983666723fbad297
Jason <jasonjones@salesforce.com>
2017-01-25 16:11:46 -0800

    Add local alias for user model

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -2,14 +2,15 @@ import { Request, Response } from 'express';
 import { UserModel } from './user.model';
 
 export class UserController {
-    private static User = new UserModel().getModel().user;
+    private static userModel = new UserModel().getModel().user;
 
     public static getUsers(req: Request, res: Response): void {
         res.json({message: 'Getting all users...'});
     }
 
     public static seedUsers(req: Request, res: Response): void {
-       UserController.User.find({}).exec()
+        let User = UserController.userModel;
+        User.find({}).exec()
             .then(collection => {
                 if (collection && collection.length === 0) {
                     // create users here...
