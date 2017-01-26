00c0c35c4d9d8c33779625d836bd294ca68c98fa
Jason <jasonjones@salesforce.com>
2017-01-25 16:11:01 -0800

    Implement pending tests

--- src/user/user.controller.test.ts
+++ src/user/user.controller.test.ts
@@ -18,7 +18,18 @@ describe('User Controller', () => {
         };
     });
 
-    it('getUsers() makes a call to res.json()');
-    it('getUsers() calls res.json() with message');
+    it('getUsers() makes a call to res.json()', () => {
+        UserController.getUsers(req, res);
+        expect(res.json.calledOnce).to.be.true;
+    });
+
+    it('getUsers() calls res.json() with message', () => {
+        let resObj = {message: 'Getting all users...'};
+
+        UserController.getUsers(req, res);
+
+        expect(res.json.calledOnce).to.be.true;
+        expect(res.json.calledWith(resObj)).to.be.true;
+    });
 
 });
