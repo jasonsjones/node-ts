37a27dfcec6666599d4120a2b44f2733af69e088
Jason <jasonjones@salesforce.com>
2017-01-26 15:09:43 -0800

    Refactor and add user controller tests

--- src/user/user.controller.test.ts
+++ src/user/user.controller.test.ts
@@ -2,8 +2,11 @@
 import * as mocha from 'mocha';
 import * as chai from 'chai';
 import * as sinon from 'sinon';
+import 'sinon-mongoose';
+import 'sinon-as-promised';
 import { Request, Response } from 'express';
 
+import { UserModel } from './user.model';
 import { UserController } from './user.controller';
 
 const expect = chai.expect;
@@ -18,18 +21,55 @@ describe('User Controller', () => {
         };
     });
 
-    it('getUsers() makes a call to res.json()', () => {
-        UserController.getUsers(req, res);
-        expect(res.json.calledOnce).to.be.true;
+    describe('getUsers()', () => {
+
+        it('calls res.json()', () => {
+            UserController.getUsers(req, res);
+            expect(res.json.calledOnce).to.be.true;
+        });
+
+        it('calls res.json() with message', () => {
+            let resObj = {message: 'Getting all users...'};
+
+            UserController.getUsers(req, res);
+
+            expect(res.json.calledOnce).to.be.true;
+            expect(res.json.calledWith(resObj)).to.be.true;
+        });
     });
 
-    it('getUsers() calls res.json() with message', () => {
-        let resObj = {message: 'Getting all users...'};
+    describe('seedUsers()', () => {
 
-        UserController.getUsers(req, res);
+        it('calls res.json()', (done) => {
+            UserController.seedUsers(req, res, function() {
+                expect(res.json.calledOnce).to.be.true;
+                done();
+            });
+        });
 
-        expect(res.json.calledOnce).to.be.true;
-        expect(res.json.calledWith(resObj)).to.be.true;
+        it('calls res.json() with message when db is empty', (done) => {
+            let resObj = {message: 'seeding users in database'};
+            let User = new UserModel().getModel().user;
+            let UserMock = sinon.mock(User);
+
+            UserMock.expects('find').withArgs({})
+                .chain('exec')
+                .resolves([]);
+
+            UserController.setModelForTesting(User);
+            UserController.seedUsers(req, res, function() {
+                UserMock.verify();
+                UserMock.restore();
+                expect(res.json.calledOnce).to.be.true;
+                expect(res.json.calledWith(resObj)).to.be.true;
+                done();
+            });
+        });
+
+        it('calls res.json() with users when db is not empty');
+
     });
+
+
 
 });
