03a62eb3e0126227fea1c8f35d40b35936cd2386
Jason <jasonjones@salesforce.com>
2017-02-01 15:09:39 -0800

    Hash pwd before saving in user model

--- src/user/user.model.ts
+++ src/user/user.model.ts
@@ -1,3 +1,4 @@
+import * as bcrypt from 'bcrypt-nodejs';
 import { Schema, Model } from 'mongoose';
 import { DbManager } from '../config/dbmanager';
 import { DbEnv } from '../common/enums';
@@ -5,10 +6,13 @@ import { IModel } from '../common/model';
 import { IUserModel } from './user.interfaces';
 
 class UserSchema {
+    static readonly SALT_WORK_FACTOR = 10;
     private schema: Schema;
 
     constructor() {
         this.defineSchema();
+        this.setupPreHooks();
+        this.setupMethods();
     }
 
     public getSchema(): Schema {
@@ -30,21 +34,56 @@ class UserSchema {
             createdDate: {type: Date, default: Date.now()}
         });
     }
+
+    private setupPreHooks(): void {
+        this.schema.pre('save', function (next) {
+            let user = this;
+
+            // early return if the password is not modified
+            if (!user.isModified('local.password')) {
+                return next();
+            }
+
+            // the password has changed so we need to hash it before saving
+            bcrypt.genSalt(UserSchema.SALT_WORK_FACTOR, function (err, salt) {
+                if (err) {
+                    return next(err);
+                }
+
+                bcrypt.hash(user.local.password, salt, null, function (err, hash) {
+                    if (err) {
+                        return next(err);
+                    }
+
+                    user.local.password = hash;
+                    next();
+                });
+            });
+        });
+    }
+
+    private setupMethods(): void {
+        this.schema.methods.verifyPassword = function (password) {
+            return bcrypt.compareSync(password, this.local.password);
+        };
+    }
 }
 
 export class UserModel {
     private model: IModel;
+    private testModel: IModel;
     private manager: DbManager;
     private schema: Schema;
 
     constructor(schema: Schema) {
         this.manager = DbManager.getInstance(DbEnv.DOCKER);
         this.schema = schema;
-        this.model = Object();
         this.initModel();
+        this.initTestModel();
     }
 
     private initModel(): void {
+        this.model = Object();
         let connection = this.manager.getConnection();
         try {
             this.model.user = connection.model<IUserModel>('User');
@@ -55,10 +94,27 @@ export class UserModel {
         }
     }
 
+    private initTestModel(): void {
+        this.testModel = Object();
+        let connection = this.manager.getTestConnection();
+        try {
+            this.testModel.user = connection.model<IUserModel>('User');
+        } catch (e) {
+            if (e.name === 'MissingSchemaError') {
+                this.testModel.user = connection.model<IUserModel>('User', this.schema);
+            }
+        }
+    }
+
     public getModel(): IModel {
         return this.model;
+    }
+
+    public getTestModel(): IModel {
+        return this.testModel;
     }
 }
 
 let userSchema: Schema = new UserSchema().getSchema();
 export const User: Model<IUserModel> = new UserModel(userSchema).getModel().user;
+export const TestUser: Model<IUserModel> = new UserModel(userSchema).getTestModel().user;
