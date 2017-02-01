89f12e9b4f9e92f858d012b0673aaa5baa18b219
Jason <jasonjones@salesforce.com>
2017-01-31 20:52:57 -0800

    Add unit tests for dbmanager

---
+++ src/config/dbmanager.test.ts
@@ -0,0 +1,25 @@
+import * as mocha from 'mocha';
+import * as chai from 'chai';
+
+import { DbEnv } from '../common/enums';
+import { DbManager } from './dbmanager';
+
+const expect = chai.expect;
+
+describe('DbManager', () => {
+    describe('getInstance()', () => {
+        it('returns an instance of DbManager', () => {
+            let dbmanager = DbManager.getInstance(DbEnv.DOCKER);
+            expect(dbmanager).to.be.instanceOf(DbManager);
+        });
+
+        it('returns a singleton of DbManager', () => {
+            let dbmanagerA = DbManager.getInstance(DbEnv.DOCKER);
+            let dbmanagerB = DbManager.getInstance(DbEnv.DOCKER);
+
+            expect(dbmanagerA).to.be.instanceOf(DbManager);
+            expect(dbmanagerB).to.be.instanceOf(DbManager);
+            expect(dbmanagerA).to.equal(dbmanagerB, 'instances should be equal');
+        });
+    });
+});
